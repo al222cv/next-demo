85c060c69530e295ad22df898f53d695c0a9fe50
Andreas Larsson <al222cv@gmail.com>
2023-02-21 22:47:54 +0100

    WIP

--- pages/env.js
+++ pages/env.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Static = () => (
   <p>
     This page presents an env variable, should be different based on
-    environment: {process.env.TEST}
+    environment: {process.env.TEST}.
   </p>
 )
 

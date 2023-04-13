cbea5980153b7b213d3fab9f714b34ce81e3c2ec
Andreas Larsson <al222cv@gmail.com>
2023-04-13 23:08:11 +0200

    Trigger build

--- __checks__/homepage.spec.js
+++ __checks__/homepage.spec.js
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test'
 
 // You can override the default Playwright test timeout of 30s
 // test.setTimeout(60_000);
-
 test('Checkly Homepage', async ({ page }) => {
   const response = await page.goto('https://checklyhq.com')
   expect(response?.status()).toBeLessThan(400)
